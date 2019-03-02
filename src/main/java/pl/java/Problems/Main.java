7b284ccfb89c6bd539a0d4ac44d91e307051fce8
Bartlomiej Lazarek <bartek.lazarek@gmail.com>
2019-03-02 12:46:34 +0100

    examples of using process

--- src/main/java/pl/java/Problems/Main.java
+++ src/main/java/pl/java/Problems/Main.java
@@ -1,8 +1,41 @@
 package pl.java.Problems;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Process process;
+
+        final String HOME_DIR = System.getProperty("user.home");
+        System.out.println(HOME_DIR);
+
+        try {
+            process = Runtime.getRuntime().exec("cmd /c dir " + HOME_DIR);
+            System.out.println(process.getOutputStream());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+
+
+            process = Runtime.getRuntime().exec("cmd /c " + HOME_DIR + "\\test.txt");
+            process = Runtime.getRuntime().exec("mspaint.exe");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+             process.destroy();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
